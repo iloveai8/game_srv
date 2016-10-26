3eaac8adbc1eaefa484ee982a0506c95407e6762
iloveai8 <iloveai8@163.com>
2016-10-26 21:03:20 +0800

    modify server

--- src/game_srv_sup.erl
+++ src/game_srv_sup.erl
@@ -7,16 +7,6 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-upgrade() ->
-    {ok, {_, Specs}} = init([]),
-    Old = sets:from_list( [Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
-    New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
-    Kill = sets:subtract(Old, New),
-
-    sets:fold(fun (Id, ok) -> supervisor:terminate_child(?MODULE, Id), supervisor:delete_child(?MODULE, Id), ok end, ok, Kill),
-    [supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
-    ok.
-
 init([]) ->
     Procs = [],
     {ok, {{one_for_one, 10, 10}, Procs}}.
